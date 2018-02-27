b0f1235bb6c83f55946dd8b9799677420afe15a6
Yoshiki <yoshiki_liwei@icloud.com>
2018-02-27 12:51:11 +0900

    add favicon.ico

--- resources/lang/zh-CN/messages.php
+++ resources/lang/zh-CN/messages.php
@@ -2,5 +2,19 @@
 
 return [
     'title' => 'Hi Japan',
-    'footer_copyright' => 'Copyright © 东京新青年 2018',
+    'footer_copyright' => 'Copyright © 東京新青年 2018',
+
+    'dashboard_title' => '状態一覧',
+    'order_title' => '訂単',
+    'outlet_title' => '物産店',
+    'used_title' => '二手',
+    'house_title' => '房屋',
+    'Invitation_title' => '招聘',
+    'gourmet_title' => '美食',
+    'question_title' => '提問',
+    'event_title' => '活動',
+    'news_title' => '新聞',
+    'user_title' => '用户',
+    'search_title' => '捜索',
+    'advertisement_title' => '広告',
 ];
